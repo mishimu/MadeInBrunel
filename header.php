14379a729265bc5b8082cac29d718a6fd83e56cf
Aaron Faber <shnist@btinternet.com>
2011-11-09 22:59:09 +0000

    removal of event content on the home page - changed id to socialMedia

--- header.php
+++ header.php
@@ -47,8 +47,20 @@
 	<script type="text/javascript" src="/wp-content/themes/mib/_js/Frutiger_LT_Std.font.js"></script>
 	<script type="text/javascript" src="/wp-content/themes/mib/_js/jquery.cycle.all.min.js"></script>
 	<script type="text/javascript" src="/wp-content/themes/mib/_js/core.js"></script>
+	<!-- google analytics -->
+	<script type="text/javascript">
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-26923120-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	</script>
     
-    <?php include('./wp-content/themes/mib/_includes/twitter-rss.php'); ?>
+	<?php //include('./wp-content/themes/mib/_includes/twitter-rss.php'); ?>
 
 <?php
 	/* We add some JavaScript to pages with the comment form
--- loop-home.php
+++ loop-home.php
@@ -38,19 +38,8 @@
                     
                     <p><a href="<?php echo get_page_link(7); ?>">Read our story</a></p>
 			</div>
-			<div id="events" class="col-third">
-				<h2>Events</h2>
-				<div class="events">
-				
-				<!-- Pecha Kucha -->
-				<p><a href="/show-and-events/pecha-kucha/"><img src="/wp-content/themes/mib/_images/sidebar_pechakucha.gif" alt="Pecha Kucha" /></a></p>
-				<p>For the fifth year running Made in Brunel and Pecha Kucha are jointly presenting a high energy, high profile series of speakers discussing the many hues of Innovation. (<a href="/show-and-events/pecha-kucha/">Read more</a>)</p>
-				
-				<!-- 8 Brand Visions -->
-				<p><a href="/show-and-events/8-brand-visions/"><img src="/wp-content/themes/mib/_images/sidebar_8brandvisions.gif" alt="8 Brand Visions" /></a></p>
-				<p>We invite you to join us to see and hear an exciting selection of ideas for the future development of eight world-famous brands, as envisaged by Brunel design students. (<a href="/show-and-events/8-brand-visions/">Read more</a>)</p>
-				</div>
-				<p><a href="/show-and-events/">View all events</a></p>
+			<div id="socialMedia" class="col-third">
+
 			</div>
 			<div id="latest-news" class="col-third no-margin">
 				<h2>Latest News <a href="<?php bloginfo('rss2_url'); ?>"><img id="latest-news-rss-icon" src="/wp-content/themes/mib/_images/site_rss-icon.gif" alt="RSS Feed" /></a></h2>
