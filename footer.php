e5b8b99626a43f6bdb0b3aa1f89478860c24ef27
Aaron Faber <shnist@btinternet.com>
2011-11-10 00:24:18 +0000

    priority footer content changes

--- footer.php
+++ footer.php
@@ -9,12 +9,16 @@
 			<div class="col-245" id="copyrights">
 				
 				<ul>
-				
-					<li><a href="/"><img class="footer-logo" src="/wp-content/themes/mib/_images/site_mib_footer_logo.gif" alt="Made in Brunel logo" border="0" /></a></li>
-
-					<li><a href="http://www.brunel.ac.uk" target="_blank"><img class="footer-logo" src="/wp-content/themes/mib/_images/site_brunel_footer_logo.png" alt="Brunel University" /></a></li>
-				
-					<li><a href="http://www.coinstreet.org" target="_blank"><img class="footer-logo" src="/wp-content/themes/mib/_images/site_bargehouse_footer_logo.png" alt="Bargehouse" /></a></li>
+					<li>
+						<a href="/">
+							<img class="footer-logo" src="/wp-content/themes/mib/_images/site_mib_footer_logo.gif" alt="Made in Brunel logo" border="0" />
+						</a>
+					</li>
+					<li>
+						<a href="http://www.brunel.ac.uk" target="_blank">
+							<img class="footer-logo" src="/wp-content/themes/mib/_images/site_brunel_footer_logo.png" alt="Brunel University" />
+						</a>
+					</li>
 				</ul>				
 				<p>&copy; Made in Brunel, <?php echo date('Y'); ?></p>
 			</div>
@@ -49,32 +53,8 @@
 					<li><a href="/sitemap.xml" target="_blank">Sitemap</a></li>
 				</ul>
 			</div>
-			<div class="col-245" id="sponsors">
-				<h2>Sponsors</h2>
-				<p>Made in Brunel is proud to be sponsored by:</p>
-				<ul>
-					<li><img src="/wp-content/themes/mib/_images/site_footer-autodesk.png" alt="Autodesk logo" /></li>
-					<li><img src="/wp-content/themes/mib/_images/site_footer-cocacola.png" alt="Coca-Cola logo" /></li>
-					<li><img src="/wp-content/themes/mib/_images/site_footer-lg.png" alt="LG Electronics logo" /></li>
-				</ul>
-			</div>
 			<div id="twitter">
-				<h2><a href="http://twitter.com/madeinbrunel11" target="_blank">@madeinbrunel</a></h2>
-				<ul id="feed">
-				<?php 
-				//call the tweets from the Twitter RSS feed
-				
-				//the first variable passed is the username, the second is number of tweets to show 
-				if($tweets = loadTwitterRSS("madeinbrunel11", 4)){
-					foreach ($tweets as $k => $v) {
-				}
-				?>
-				<li id="tweet-<?php echo $k; ?>" class="tweet"><?php echo $v['desc'];?>
-				<br /><span class="tweetdate"><?php echo $v['date']; ?></span></li>
-				<?php
-				}
-				?>
-				</ul>
+				<h2><a href="http://twitter.com/madeinbrunel12" target="_blank">@madeinbrunel12</a></h2>
 			</div>
 		</div>
 	</div>
